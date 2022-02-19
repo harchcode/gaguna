750f4074ebea4bf007812056098d40cd09d09134
Hartono Chandra <harchcode@gmail.com>
2022-02-19 14:10:00 +0700

    feat: add more utils

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export * from "./math";
+export * from "./easings";
+export * from "./pool";
+export * from "./intersections";
+export * from "./observable";
+export * from "./tween";
--- src/math.ts
+++ src/math.ts
@@ -17,3 +17,19 @@ export function getRandomIntInclusive(min: number, max: number) {
 
   return Math.floor(Math.random() * (maxInt - minInt + 1) + minInt); //The maximum is inclusive and the minimum is inclusive
 }
+
+export function nextPowerOf2(value: number): number {
+  if (value <= 0) return 1;
+
+  let result = value;
+
+  result--;
+  result |= result >> 1;
+  result |= result >> 2;
+  result |= result >> 4;
+  result |= result >> 8;
+  result |= result >> 16;
+  result++;
+
+  return result;
+}
