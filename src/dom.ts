09913ae632ec57f43c6013d64c7ff181606da863
Hartono Chandra <harchcode@gmail.com>
2022-12-27 09:30:14 +0700

    update

--- src/dom.ts
+++ src/dom.ts
@@ -12,14 +12,14 @@ export function scaleDisplayToWindow(
       ? window.innerHeight / height
       : window.innerWidth / width;
 
-  mainElement.style.width = `${width * scale}px`;
-  mainElement.style.height = `${width * scale}px`;
+  mainElement.style.width = `${width}px`;
+  mainElement.style.height = `${height}px`;
+  mainElement.style.transform = `scale(${scale})`;
 
   if (!fullWindowElement) return;
 
   fullWindowElement.style.width = `${window.innerWidth / scale}px`;
   fullWindowElement.style.height = `${window.innerHeight / scale}px`;
-  fullWindowElement.style.transformOrigin = "0 0";
   fullWindowElement.style.transform = `scale(${scale})`;
 }
 
@@ -35,4 +35,16 @@ export function cxo(classNames: Record<string, boolean | undefined | null>) {
   return Object.keys(classNames)
     .filter(k => classNames[k])
     .join(" ");
+}
+
+// Just normal string with html tag, because the lit-html vscode plugin only recognize this tagged string
+// and not normal string as html
+export function html(strings: TemplateStringsArray, ...values: unknown[]) {
+  let str = strings[0];
+
+  for (let i = 0; i < values.length; i++) {
+    str += values[i] + strings[i + 1];
+  }
+
+  return str;
 }
